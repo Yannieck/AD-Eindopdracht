5616467dfa4e97a4545bbb6ddb15b5b4b13d0e6d
Yannieck Blaauw <yannieck.blaauw@student.nhlstenden.com>
2023-03-13 13:13:00 +0100

    added stack push

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import org.example.datastructures.Stack;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Stack stack = new Stack();
+        stack.print();
+        stack.push("e");
+        stack.print();
     }
 }
